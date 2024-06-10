2b9ddb09d7933468027ed12d1a9dfcd73f01bef2
JuleTrotzki <tj4062@mci4me.at>
2024-06-10 12:00:52 +0200

    erster Entwurf

---
+++ ekgdata_jule.py
@@ -0,0 +1,162 @@
+import json
+import pandas as pd
+import plotly.graph_objs as go
+
+
+
+# %% Objekt-Welt
+
+# Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
+
+class EKGdata:
+
+## Konstruktor der Klasse soll die Daten einlesen
+
+    def __init__(self, ekg_dict):
+        pass
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
+    
+    @staticmethod  
+    def load_ekg_data():
+        file = open("data/ekg_data")
+        ekg_data = json.load(file)
+        return ekg_data
+    
+    def load_by_id(test_id):
+        with open("data/person_db.json") as file:
+            ekg_data = json.load(file)
+            for person in ekg_data:
+                for ekg_test in person["ekg_tests"]:
+                    if ekg_test["id"] == test_id:
+                        return ekg_test
+        return {}
+
+
+    @staticmethod  
+    def find_peaks(series, threshold, respacing_factor=5):
+   
+        # Respace the series
+        series = series.iloc[::respacing_factor]
+     
+        # Filter the series
+        series = series[series>threshold]
+        
+        peaks = []
+        last = 0
+        current = 0
+        next = 0
+
+        for index, row in series.items():
+            last = current
+            current = next
+            next = row
+
+            if last < current and current > next and current > threshold:
+                peaks.append(index-respacing_factor)
+
+        return peaks
+    
+   # def calculate_HR(peaks, respacing_factor=5, sampling_rate=1000):
+    #    peak_intervals = pd.Series(peaks).diff()
+        #peak_intervals_seconds = peak_intervals / (sampling_rate / respacing_factor) #in sekunden
+     #   heart_rates = 60 / peak_intervals
+      #  heart_rates = heart_rates.dropna()*2
+    
+       # df = pd.DataFrame({'Zeitpunkt': peaks[1:], 'Herzfrequenz': heart_rates})
+    
+        #return df
+        
+    def calculate_HR(peaks, sampling_rate=1000, smooth_window_size=3):
+    
+        peak_intervals = pd.Series(peaks).diff().iloc[1:] / sampling_rate
+        heart_rates = 60 / peak_intervals
+        
+        # Glättung der Herzfrequenzdaten durch Moving Average
+        smoothed_heart_rates = heart_rates.rolling(window=smooth_window_size, min_periods=1).mean()
+    
+        timepoints = peaks[1:]
+    
+        df = pd.DataFrame({'Zeitpunkt': timepoints, 'Herzfrequenz': smoothed_heart_rates})
+        
+        return df
+    
+    
+
+    def plot_time_series(df, peaks, start_index=None, end_index=None):
+        
+        if start_index is None:
+            start_index = 0
+        if end_index is None:
+            end_index = len(df)
+        
+    
+        df = df.iloc[start_index:end_index]
+        
+        peaks_in_range = [peak for peak in peaks if start_index <= peak < end_index]
+        peak_times = df.iloc[peaks_in_range]['Time in ms']
+        peak_values = df.iloc[peaks_in_range]['EKG in mV']
+        
+        fig = go.Figure()
+
+        # EKG-Daten plotten
+        fig.add_trace(go.Scatter(
+            x=df['Time in ms'], 
+            y=df['EKG in mV'],
+            mode='lines',
+            name='EKG Data'))
+
+        # Peaks hinzufügen
+        fig.add_trace(go.Scatter(
+            x=peak_times,
+            y=peak_values,
+            mode='markers',
+            marker=dict(color='red', size=5),
+            name='Peaks'))
+
+        # Layout anpassen
+        fig.update_layout(
+            title='EKG Data',
+            xaxis_title='Time in ms',
+            yaxis_title='EKG in mV',
+            showlegend=True)
+
+        return fig
+    
+if __name__ == "__main__":
+    print("This is a module with some functions to read the EKG data")
+    file = open("data/person_db.json")
+    person_data = json.load(file)
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    print(ekg_dict)
+    ekg = EKGdata(ekg_dict)
+    print(ekg.df.head())
+    
+    # Beispielaufruf der Methode load_by_id
+    test_id = 1
+    ekg_test_data = EKGdata.load_by_id(test_id)
+
+    if ekg_test_data:
+        print("Gefundene EKG-Testdaten:")
+        print(ekg_test_data)
+    else:
+        print("EKG-Test mit der angegebenen ID wurde nicht gefunden.")
+        
+    # Beispielaufruf find_peaks
+    file_path = "data/ekg_data/01_Ruhe.txt"
+    ekg_data = pd.read_csv(file_path, sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
+    threshold = 345
+    peaks = EKGdata.find_peaks(ekg_data['EKG in mV'], threshold)
+    print("Gefundene Peaks:", peaks)
+    fig = EKGdata.plot_time_series(ekg_data, peaks)
+    fig.show()
+    
+    # Beispielaufruf estimate_HR
+    heart_rate = EKGdata.calculate_HR(peaks)
+    print("Geschätzte Herzfrequenz:", heart_rate, "Schläge pro Minute")
+    
+    
+
+    
---
+++ main_jule.py
@@ -0,0 +1,87 @@
+import streamlit as st
+from person import Person
+from ekgdata_jule import EKGdata
+import plotly.graph_objs as go
+
+
+if 'threshold' not in st.session_state:
+    st.session_state.threshold = 345
+if 'start_index' not in st.session_state:
+    st.session_state.start_index = 0
+if 'end_index' not in st.session_state:
+    st.session_state.end_index = 10000
+if 'sampling_rate' not in st.session_state:
+    st.session_state.sampling_rate = 1000
+if 'smooth_window_size' not in st.session_state:
+    st.session_state.smooth_window_size = 100
+
+
+
+# Laden der Personendaten
+person_data = Person.load_person_data()
+person_list = Person.get_person_list(person_data)
+
+# Titel der App
+st.title("EKG Data Analysis App")
+
+# Auswahl der Person
+selected_person_name = st.selectbox("Wähle eine Person aus:", person_list)
+selected_person_data = Person.find_person_data_by_name(selected_person_name)
+
+# Anzeige der Personendaten
+if selected_person_data:
+    person = Person(selected_person_data)
+    
+    if person.picture_path:
+        col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image(person.picture_path,caption=f"{person.firstname} {person.lastname}", width=200)
+    with col2:
+        st.write(f"Name: {person.firstname} {person.lastname}")
+        st.write(f"Geburtsjahr: {person.date_of_birth}")
+        st.write(f"Maximale Herzfrequenz: {person.calc_max_heart_rate()} bpm")
+    
+    # Auswahl des EKG-Tests
+    ekg_test_list = [f"Test ID: {test['id']} - Datum: {test['date']}" for test in selected_person_data["ekg_tests"]]
+    selected_ekg_test = st.selectbox("Wähle einen EKG-Test aus:", ekg_test_list)
+    
+    if selected_ekg_test:
+        selected_test_id = int(selected_ekg_test.split()[2])
+        ekg_test_data = EKGdata.load_by_id(selected_test_id)
+        
+        if ekg_test_data:
+            ekg = EKGdata(ekg_test_data)
+            
+            # Ermittlung der Peaks
+            peaks = EKGdata.find_peaks(ekg.df['EKG in mV'], st.session_state.threshold)
+            
+            # Berechnung der Herzfrequenz
+            heart_rates_df = EKGdata.calculate_HR(peaks, st.session_state.sampling_rate, st.session_state.smooth_window_size)
+            
+            # Plot der EKG Daten
+            fig = EKGdata.plot_time_series(ekg.df, peaks, st.session_state.start_index, st.session_state.end_index)
+            st.plotly_chart(fig)
+            
+            # Plot der Herzfrequenz
+            heart_rate_fig = go.Figure()
+            heart_rate_fig.add_trace(go.Scatter(
+                x=heart_rates_df['Zeitpunkt'], 
+                y=heart_rates_df['Herzfrequenz'],
+                mode='lines',
+                name='Herzfrequenz'))
+                
+            heart_rate_fig.update_layout(
+                title='Herzfrequenz über die Zeit',
+                xaxis_title='Zeitpunkt (ms)',
+                yaxis_title='Herzfrequenz (Schläge pro Minute)',
+                showlegend=True)
+            st.plotly_chart(heart_rate_fig)
+            
+            # Eingabe threshold, start_index und end_index
+            st.session_state.threshold = st.slider("Wähle den Schwellenwert für die Peaks:", min_value=0, max_value=1000, value=345)
+            st.session_state.end_index = st.slider("Wähle die Länge des zu betrachtenden Zeitbereichs:", min_value=0, max_value=len(ekg.df), value=10000)
+            st.session_state.sampling_rate = st.slider("Wähle die Abtastrate:", min_value=1, max_value=10000, value=1000)
+            st.session_state.smooth_window_size = st.slider("Wähle die Auflösung der Herzfrequenz-Anzeige:", min_value=1, max_value=10000, value=100)
+ 
+else:
+    st.write("Keine Daten für die ausgewählte Person gefunden.")
